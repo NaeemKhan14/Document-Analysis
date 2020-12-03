d650455856549ff10d1a041034cb8b80e70e1173
Naeem Khan <naeemukhan14@gmail.com>
2020-12-04 02:30:52 +0400

    Task 8 completed.

--- Main.py
+++ Main.py
@@ -1,20 +1,71 @@
 from DataHandler import DataHandler
 from GraphHandler import GraphHandler
 from GUIHandler import GUIHandler
+import argparse
 
-#graph = GraphHandler("sample_3m_lines.json")
-#g = graph.show_likes_graph("140109173556-a4b921ab7619621709b098aa9de4d736")
 
-gui = GUIHandler()
-gui.mainloop()
+if __name__ == '__main__':
+    # Set up arguments
+    parser = argparse.ArgumentParser(description='Document Helper for analysing data.')
+    parser.add_argument("-u", "--visitor_uuid", help="Visitor UUID")
+    parser.add_argument("-d", "--doc_uuid", help="Document UUID")
+    parser.add_argument("-t", "--task_id", help="Task ID")
+    parser.add_argument("-f", "--file_name", help="File Name")
 
-# data = DataHandler('sample_100k_lines.json')
-# top_readers = data.get_top_ten_likes("100806162735-00000000115598650cb8b514246272b5")
-# print(top_readers)
-#
-#graph = GraphHandler("issuu_cw2.json")
-#
-#graph.get_country_graph("140213232558-bdd53a3a2ae91f2c5f951187668edd50")
-# graph.get_continent_graph()
-# graph.get_browser_data_graph()
-# graph.get_browser_names_graph()
+    args = parser.parse_args()
+    # File name is required, so every condition needs to check for this.
+    if args.file_name:
+        if args.task_id == '2a':
+            if args.doc_uuid:  # If document uuid is provided.
+                graph = GraphHandler(args.file_name)
+                graph.get_country_graph(args.doc_uuid)
+            else:
+                print('No document uuid provided. Please use -h for more help.')
+        elif args.task_id == '2b':
+            if args.doc_uuid:
+                graph = GraphHandler(args.file_name)
+                graph.get_country_graph(args.doc_uuid)
+                graph.get_continent_graph()
+            else:
+                print('No document uuid provided. Please use -h for more help.')
+        elif args.task_id == '3a':
+            graph = GraphHandler(args.file_name)
+            graph.get_browser_data_graph()
+        elif args.task_id == '3b':
+            graph = GraphHandler(args.file_name)
+            graph.get_browser_names_graph()
+        elif args.task_id == '4d':
+            data = DataHandler(args.file_name)
+            rank = 1
+            print('Rank    Visitor ID      Hours Spent Reading')
+            for values in data.get_top_ten_likes().iteritems():
+                print('{rank}    {visitor_uuid}     {read_time}'.format(rank=rank,
+                                                                        visitor_uuid=values[0],
+                                                                        read_time=int(values[1])))
+                rank += 1
+        elif args.task_id == '5':
+            if args.doc_uuid:
+                data = DataHandler(args.file_name)
+                results = data.get_top_ten_likes(args.doc_uuid, args.visitor_uuid)
+                rank = 1
+                print('Rank    Visitor ID                    Document ID')
+                for values in data.get_top_ten_likes(args.doc_uuid, args.visitor_uuid).iteritems():
+                    print('{rank}    {visitor_uuid}     {doc_uuid}'.format(rank=rank,
+                                                                           visitor_uuid=values[0][0],
+                                                                           doc_uuid=values[0][1]))
+                    rank += 1
+            else:
+                print('No document uuid provided. Please use -h for more help.')
+        elif args.task_id == '6':
+            if args.doc_uuid:
+                graph = GraphHandler(args.file_name)
+                graph.show_likes_graph(args.doc_uuid, args.visitor_uuid)
+            else:
+                print('No document uuid provided. Please use -h for more help.')
+
+    else:
+        # If file is not provided, that means we are not opening
+        # the application from command line, and open the GUI instead.
+        print('No file name given. Please use -h for more help.')
+        gui = GUIHandler()
+        gui.mainloop()
